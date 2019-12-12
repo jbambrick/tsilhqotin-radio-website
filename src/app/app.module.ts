19fb37e84084e4a5447806794d2d19e53e637e56
unknown <jbambrick@tsilhqotinca>
2019-12-12 10:41:16 -0800

    added additional components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
