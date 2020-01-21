11c26d76753892013031f7e15218afeb299511a1
unknown <jbambrick@tsilhqotinca>
2020-01-21 14:59:23 -0800

    added contact component and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
 import { StaffComponent } from './staff/staff.component';
 import { LanguageComponent } from './language/language.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 
 const routes: Routes = [
@@ -12,6 +14,7 @@ const routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: 'staff', component: StaffComponent},
   {path: 'language', component: LanguageComponent},
+  {path: 'contact', component: ContactComponent},
 ];
 
 @NgModule({
