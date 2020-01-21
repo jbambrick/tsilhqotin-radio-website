889e1b60c43f768b3066bee9bffc62dd48159281
unknown <jbambrick@tsilhqotinca>
2020-01-21 14:37:37 -0800

    added routes to the language component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
 import { StaffComponent } from './staff/staff.component';
+import { LanguageComponent } from './language/language.component';
 
 
 const routes: Routes = [
   {path: '', component: MainComponent},
   {path: 'about', component: AboutComponent},
   {path: 'staff', component: StaffComponent},
+  {path: 'language', component: LanguageComponent},
 ];
 
 @NgModule({
